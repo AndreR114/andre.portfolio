b2d1e0ec673fda40de64249b748288489bdc03c9
AndreR114 <129870218+AndreR114@users.noreply.github.com>
2023-04-05 20:46:37 +0200

    Update ws.js

--- ws.js
+++ ws.js
@@ -1,105 +1,108 @@
-"use strict";
-/*Selecting point elements*/
+"use strict"
 
-const score0Element = document.querySelector("#playerPoints0");
+//Selceting the Elements
 
-const score1Element = document.getElementById("playerPoints1");
+const player0Element = document.querySelector(".player0");
+const player1Element = document.querySelector(".player1");
 
-const activePlayer0 = document.querySelector(".player00");
-const activePlayer1 = document.querySelector(".player01");
+const playerPunkteElement0 = document.querySelector(".punkteText0");
+const playerPunkteElement1 = document.querySelector(".punkteText1");
 
-const btnRoll = document.querySelector(".roll");
-const btnHold = document.querySelector(".hold");
-const btnReset = document.querySelector(".newGame");
+const playerCurrent0 = document.querySelector(".currentPunkteP0");
+const playerCurrent1 = document.querySelector(".currentPunkteP1");
 
-const currentElement1 = document.querySelector("#currentPointsP1");
-const currentElement0 = document.querySelector("#currentPointsP0");
-
-/*Selecting the dice and create a dice point container*/
+const btnNewGameElement = document.querySelector(".newGame");
+const btnRollElement = document.querySelector(".roll");
+const btnHoldElement = document.querySelector(".hold");
 
 const diceElement = document.querySelector("#dice");
-let currentPoints = 0;
-let scores = [0, 0];
 
-/*Variable for the active player*/
+//Start attributes
 
+let gameStarted = false;
 let activePlayer = 0;
+let currentPoints;
 
-//switching player function
+diceElement.classList.add("hide");
+
+//Switchplayer Funktion
 
 const switchPlayer = function () {
-  document.querySelector("#currentPointsP" + activePlayer).textContent = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
   currentPoints = 0;
-  activePlayer0.classList.toggle("active");
-  activePlayer1.classList.toggle("active");
+  document.querySelector(".currentPunkteP" + activePlayer).textContent =
+    currentPoints;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0Element.classList.toggle("playerActive");
+  player1Element.classList.toggle("playerActive");
 };
 
-/*Starting conditions*/
-diceElement.classList.add("hidden");
-let gameStartet = false;
+//Savecontainer for the pointerEvents
 
-//Reset the game to the starting conditions, the dice stays visible or just start the game
-btnReset.addEventListener("click", function () {
-  diceElement.classList.remove("hidden");
-  score0Element.textContent = 0;
-  score1Element.textContent = 0;
-  score0Element.textContent = 0;
-  score1Element.textContent = 0;
+let scores = [0, 0];
+
+//Handle clickelements
+
+btnNewGameElement.addEventListener("click", function () {
+  //Reset the game
+  gameStarted = true;
   currentPoints = 0;
-  currentElement0.textContent = currentPoints;
-  currentElement1.textContent = currentPoints;
-  gameStartet = true;
+  scores = [0, 0];
+  playerPunkteElement0.textContent = 0;
+  playerPunkteElement1.textContent = 0;
+  playerCurrent0.textContent = currentPoints;
+  playerCurrent1.textContent = currentPoints;
+
+  player0Element.classList.add("playerActive");
+  player1Element.classList.remove("playerActive");
+
   document
-    .querySelector(".player0" + activePlayer)
+    .querySelector(".player" + activePlayer)
     .classList.remove("playerWinner");
   activePlayer = 0;
-  activePlayer0.classList.add("active");
-  activePlayer1.classList.remove("active");
-  scores = [0, 0];
+
+  //show the dice
+
+  diceElement.classList.remove("hide");
 });
 
-/*Rolling dice functions*/
-
-btnRoll.addEventListener("click", function () {
-  //Check if game is started
-  if (gameStartet) {
-    //Generate a random dice Number
-    const dice = Math.trunc(Math.random() * 6) + 1;
-    //Display the dice
-    diceElement.src = "dice-" + dice + ".png";
-
-    //Check if player rolled a 1, if yes switch player
-    if (dice !== 1) {
-      //Add dice number to current score
-      currentPoints += dice;
-      document.querySelector("#currentPointsP" + activePlayer).textContent =
+btnRollElement.addEventListener("click", function () {
+  //create random Number
+  if (gameStarted) {
+    const randomDiceNr = Math.trunc(Math.random() * 6) + 1;
+    diceElement.src = "dice-" + randomDiceNr + ".png";
+    //check if tis a 1 and if its true switchplayer
+    if (randomDiceNr !== 1) {
+      currentPoints += randomDiceNr;
+      document.querySelector(".currentPunkteP" + activePlayer).textContent =
         currentPoints;
     } else {
       //Switch player
       switchPlayer();
     }
   } else {
-    alert("Start a new game first!");
+    alert("Start an new game first!");
   }
 });
 
-btnHold.addEventListener("click", function () {
-  //add points to the actplayer one
-  scores[activePlayer] += currentPoints;
-  document.querySelector("#playerPoints" + activePlayer).textContent =
-    scores[activePlayer];
-  //Check if score is not 100
-  if (scores[activePlayer] >= 20) {
-    document
-      .querySelector(".player0" + activePlayer)
-      .classList.add("playerWinner");
-    document
-      .querySelector(".player0" + activePlayer)
-      .classList.remove("active");
-    gameStartet = false;
+//handling the hold event
+
+btnHoldElement.addEventListener("click", function () {
+  if (gameStarted) {
+    //add points to active player and switch player
+    score[activePlayer] += currentPoints;
+    document.querySelector(".punkteText" + activePlayer).textContent =
+      score[activePlayer];
   } else {
-    //Switching player
+    alert("Start a new game first");
+  }
+  //check if points are 100
+  if (score[activePlayer] >= 100) {
+    document
+      .querySelector(".player" + activePlayer)
+      .classList.add("playerWinner");
+    gameStarted = false;
+  } else {
     switchPlayer();
   }
+  //show winner and deactivate hold and roll button
 });
